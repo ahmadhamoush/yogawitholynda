422fcf83ef43afbcaa331d6b749171809e71469c
Ahmad Hamoush <hamoush@outlook.com>
2023-02-27 01:22:08 +0200

    fixed root directory endpoint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    trailingSlash: true,
-    assetPrefix: 'http://localhost:3000',
+    reactStrictMode: true,
     env: {
         MONGODB_URL: 'mongodb+srv://hamoush:hmtGSXL1qPtgLijl@cluster0.jy0bral.mongodb.net/yogawitholynda?retryWrites=true&w=majority',
         NEXTAUTH_SECRET: 'oLyNdA213$'
@@ -9,7 +8,8 @@ module.exports = {
     images: {
         domains: [
             'http://localhost:3000',
-            'https://yogawitholynda.vercel.app/'
+            'https://yogawitholynda.vercel.app',
+            'https://charming-brigadeiros-431611.netlify.app'
         ],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
