909239a66d3dda26893bad925f3fbc07e60defb0
Ahmad Hamoush <hamoush@outlook.com>
2023-02-26 19:09:23 +0200

    Changed mongodb cluster

--- next.config.js
+++ next.config.js
@@ -2,12 +2,13 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        MONGODB_URL: 'mongodb+srv://hamoush:hmtGSXL1qPtgLijl@cluster0.evplv1k.mongodb.net/yogawitholynda?retryWrites=true&w=majority',
+        MONGODB_URL: 'mongodb+srv://hamoush:hmtGSXL1qPtgLijl@cluster0.jy0bral.mongodb.net/yogawitholynda?retryWrites=true&w=majority',
         NEXTAUTH_SECRET: 'oLyNdA213$'
     },
     images: {
         domains: [
             'http://localhost:3000',
+            'https://yogawitholynda.vercel.app/'
         ],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
