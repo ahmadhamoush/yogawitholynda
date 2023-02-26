e7d7de1d35984465ffa1a9bad625120716ec8ac3
Ahmad Hamoush <hamoush@outlook.com>
2023-02-26 20:11:58 +0200

    fixed cart  api endpoint

--- src/pages/api/cart.js
+++ src/pages/api/cart.js
@@ -2,6 +2,13 @@ import Product from "models/Product"
 import { initMongoose } from "lib/mongoose";
 
 export default async function handler(req, res) {
+
+    if (req.method !== 'POST') {
+        res.status(405).send({ message: 'Only POST requests allowed' })
+        return
+    }
+
+
     await initMongoose()
     const { ids } = req.query
     const idsArr = ids.split(',')
