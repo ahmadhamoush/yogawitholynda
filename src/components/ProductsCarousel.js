fcaee8f06be6962ff2bacd0cafdbf7146c8deb06
Ahmad Hamoush <hamoush@outlook.com>
2023-02-27 16:30:34 +0200

    added link for product carousel

--- src/components/ProductsCarousel.js
+++ src/components/ProductsCarousel.js
@@ -8,7 +8,7 @@ function ProductsCarousel ({products}){
         <div className="productsCarousel">
         <div  className="box">
         {products.filter(product=>product.category==='yoga-mats-bags').map((product,i)=>{
-            return  <Link key={product._id} href={`/product/${product.name}`}>
+            return  <Link key={product._id} href='/product/[productId]' as={`/product/${encodeURIComponent(props.name)}`}>
             <span style={{ "--i": i+1 }}>
                 <Image className="carouselImg" alt={product.name} src={product.image} width={300} height={300}/>
                 </span></Link>
